ca2d1d3a30a16e76613748eed113cb7149f3a096
Paul Lezeau <paul.lezeau@hotmail.fr>
2024-03-04 09:24:58 +0000

    WIP: added descent algorithm. Currently fixing bugs.

--- berkovich.jl
+++ berkovich.jl
@@ -61,6 +61,10 @@ function big_metric(b1::BerkovichPoint, b2::BerkovichPoint)
     return d1+d2
 end
 
+function valuation_coeff(f::PolyRingElem{T}, i) where T <: RingElement
+    return valuation(coeff(f, i))
+end
+
 function berkovichValuation(f::PolyRingElem{T}, a, r) where T <: RingElement
     """berkovichValuation(f, a, r, p) returns the valuation of polynomial f over Q_p at a point of type II or III corresponding to the ball B(a,r)
 
@@ -78,16 +82,18 @@ function berkovichValuation(f::PolyRingElem{T}, a, r) where T <: RingElement
     if r == 0
         return padic_abs(f(a))
     else
-        m = typemax(Float)
+        m = typemax(Float64)
         # consider the polynomial h = f(t+a) since the the m-th coefficient of h is the same as the m-th coefficient of g (expanded around t = a)
-        h = compose(f, a + T)
-        e = log(r, 1/a.parent().prime()) 
+        t = gen(f.parent)
+        h = compose(f, t + a)
+        p = Float64(Nemo.prime(a.parent))
+        e = - log(r, p) 
         # the max taken wrt absolute values corresponds to the min wrt valuations
         for i in 0:degree(f)
-            m = min(m, h.valuation_of_coefficient(i) + i*e)
+            m = min(m, valuation_coeff(h, i) + i*e)
         end
         # convert result back to absolute value
-        return Float(prime(a.parent))^(-m)
+        return Float64(Nemo.prime(a.parent))^(-m)
     end
 end
 
@@ -177,23 +183,6 @@ function path(b1, b2, t)
     end
 end
 
-#######################################################
-# A Few tests
-
-Q = PadicField(3, 10)
-s1 = BerkovichPoint(Q(3), 0.0)
-s2 = BerkovichPoint(Q(2), 0.5)
-R, x = polynomial_ring(Q, "x")
-f = x+1
-
-println(berkovichValuation(f, s1.center, s1.radius))
-println(path(s1, s2, 0))
-
-println(path(s1, s2, 7))
-     
-# TODO: translate the rest of the code below to Julia  
-    
-
 function abs_path_values(f, b1, b2, t1, t2, num)
     """ abs_path_values(f, b1, b2, t1, t2, num) returns two arrays: linspace(t1, t2, num) and the 
     image of linspace(t1, t2, num) by the map sending t to the valuation of f at the point attained at 
@@ -222,7 +211,7 @@ function abs_path_values(f, b1, b2, t1, t2, num)
 end
     
 
-function abs_path_values_sum(s, b1, b2, t1, t2, num)
+#= function abs_path_values_sum(s, b1, b2, t1, t2, num)
     """ abs_path_values_sum(s, b1, b2, t1, t2, num) returns two arrays: linspace(t1, t2, num) and the 
     image of linspace(t1, t2, num) by the map sending t to the valuation of f = \sum f_i at the point attained at 
     time t on the path [p1, p2] where s = [f_1, ..., f_n]
@@ -248,7 +237,7 @@ function abs_path_values_sum(s, b1, b2, t1, t2, num)
         a .+= im
     end
     return l, a
-end
+end =#
 
 function gt(b1::BerkovichPoint, b2::BerkovichPoint)
     """gt(b1, b2) returns true if the Berkovich point b1 is greater than b2 in the sense that the ball b2 is strictly contained in the ball b1.
@@ -278,7 +267,7 @@ end
 
 
 
-function dir_deriv(f::PolyRingElem{T}, b1::BerkovichPoint, b2::BerkovichPoint)
+function dir_deriv(f::PolyRingElem{T}, b1::BerkovichPoint, b2::BerkovichPoint) where T <: RingElement
     """ dir_deriv(f, b1, b2) returns the directional derivative of the function |f(x)|_p at the point b1 in the direction b2.
 
     Parameters
@@ -344,7 +333,155 @@ function dir_deriv(f::PolyRingElem{T}, b1::BerkovichPoint, b2::BerkovichPoint)
         # if we want the derivative with respect to the radius metric then this is n*|c_n|*r^{n-1}
         return -n * (p^(-m))   
     end
+end 
 
 
+#todo: make this implementation smarter. This should also test whether the point has type II or III.
+function enumerate_centers(P::BerkovichPoint)
+    """enumerate_centers(P) returns a list of elements of the padic field such that represent the branches below P
+    """
+    # If P is of type I, then the only center is P itself (as a K-point)
+    if P.radius == 0
+        return [P.center]
+    else
+        p = Nemo.prime(P.center.parent)
+        # This isn't very elegant, but it works for now. We find the integer m such that the radius of P is p^(-m). This needs to be an integer so we can raise p to the power of it.
+        m = Int64(floor(log(Float64(p), P.radius)))
+        #otherwise, if the radius is p^m then we take the centers to be the points of the form P.center + p^(-m)*i for i in 0, 1, ..., p^m
+        a = P.center
+        A = [a]
+        for i in 1:(p-1)
+            a = a + p^(-m)
+            push!(A, a)
+        end
+        return A
+    end
+end
+
+function max_radius(F, p)
+    """max_radius(F, p) returns the maximum of the absolute values of the coefficients of the polynomials in F
+    """
+    m = typemax(Float64)
+    for f in F
+        for i in 0:degree(f)
+            m = min(m, valuation_coeff(f, i))
+        end
+    end
+    m = Float64(p)^(-m)
+    return m
+end
+
+#TODO: check this function. Also it would be useful to have it output extra information e.g. the regime of f at each point, etc. 
+function enumerate_change_points(f::PolyRingElem{R}, P::BerkovichPoint, regime=degree(f)) where R <: RingElement
+    """enumerate_change_points(F, P) returns the list of points along the path from the center of P to P for which the valuation of the polynomial f changes degree (as a function of the radius). The regime parameter allows the user to specify that we are only interested in the r+1 first coefficients of f - this is useful since when the radius is small, the higher degree coefficients are not important as they won't change the valuation, and we can use this to speed up calculations.
+    """
+    # r is the index we are currently considering.
+    r = regime 
+    # Initialise the list of change points.
+    changes = []
+    p = Nemo.prime(P.center.parent)
+    # Find the valuation corresponding to the radius of P
+    val = -Int64(floor(log(Float64(p), P.radius)))
+    loop_count = 0
+    new_r = r
+    T = gen(f.parent)
+    # consider the polynomial h = f(t+a) since the the m-th coefficient of h is the same as the m-th coefficient of g (expanded around t = a)
+    f = compose(f, T+P.center)
+    println(f)
+    while r != 0
+        loop_count += 1
+        println("r = ", r, " val = ", val)
+        new_val = valuation_coeff(f, r) + val 
+        for j in (r-1):-1:0
+            println("r = ", r, " j = ", j)
+            # if the coefficient is 0 we can ignore it and move on to the next interation of the loop.
+            if coeff(f, j) != 0
+                # otherwise we compute at which radius a jump from coeff r to coeff j would occur (i.e. for which radius s do we have S
+                # |coeff(j)| => |coeff(r)|*p^(r-j). 
+                temp_val = (valuation_coeff(f, j) - valuation_coeff(f, r))/(r-j)
+                #println(temp_val, " ", val)
+                # if this radius is smaller than the current radius and larger than the current jump radius we update it.
+                if temp_val >= val && temp_val <= new_val
+                    new_val = temp_val
+                    # Update the regime of f at that point.
+                    new_r = j
+                    #println(new_r)
+                end
+            end 
+        end
+        # if r has changed then we must have found a change point above so we can add it to the list
+        if r != new_r
+            push!(changes, BerkovichPoint(P.center, Float64(p)^(-val)))
+            r = new_r
+            val = new_val
+        # otherwise the modulus of f is constant for the rest of the branch so we can stop.
+        else 
+            println("Break ", r)
+            #println(loop_count)
+            break
+        end
+    end 
+    return changes
+end
+
+# Naive implementation of the tree search algorithm. Still need to implements some of the dependencies plus add some of the 
+# branch cutting mechanisms.
+function min_find(F, N, R) #= ::Vector{PolyRingElem{T}} =# #= where T <: RingElement =#
+    """min_find(F, N, R) returns a set type II points of lying above the roots of the polynomials in F.  
+    N is the number of iterations we allow and R is the p-adic field (R should really be removed since we should be able to access it from the polynomials in F). 
+    """
+    p = Nemo.prime(R)
+    # Initialise set of type II points  
+    B = [BerkovichPoint(R(0), max_radius(F, p))]
+    for i in 1:N
+        # Initialise new set of type II points
+        C = []
+        for b in B
+            #println(B)
+            # Enumerate the centers of the branches below p
+            L = enumerate_centers(b)
+            for a in L
+                println(a)
+                for f in F
+                    # consider the polynomial h = f(t+a) since the the m-th coefficient of h is the same as the m-th coefficient of g (expanded around t = a)
+                    T = gen(f.parent)
+                    h = compose(f, a + T)
+                    #add all change points to the new set of type II points
+                    append!(C, enumerate_change_points(h, BerkovichPoint(a, b.radius)))
+                end 
+            end
+        end
+        # Update the set of type II points
+        B = C
+    end 
+    return B
+end
 
 
+################### UNIT TESTS #################### 
+
+
+# Define a few objects
+Q = PadicField(3, 10)
+s1 = BerkovichPoint(Q(3), 0.0)
+s2 = BerkovichPoint(Q(2), 0.5)
+R, x = polynomial_ring(Q, "x")
+f = (x-3)*(x-12)
+#println(compose(f, f))
+
+#######################################################
+#= 
+# Compute some paths
+println(berkovichValuation(f, s1.center, s2.radius))
+println(path(s1, s2, 0))
+println(path(s1, s2, 7))    
+
+# List the centers of the branches below s
+println(enumerate_centers(BerkovichPoint(Q(3), 1)))
+println(enumerate_centers(BerkovichPoint(Q(3), 0)))  =#
+
+println(enumerate_change_points(f, BerkovichPoint(Q(3), 3)))
+
+# Find the minimum of a polynomial
+#println(max_radius([f], 3))
+#println(min_find([f], 10, Q))
