5dfc07d25151c9d911b3fba913621dfcb302f2d7
Paul-Lez <paul.lezeau@gmail.com>
2025-10-17 17:52:14 +0100

    Allow greedy descent to do one step at a time

--- src/optim/gradient_descent.jl
+++ src/optim/gradient_descent.jl
@@ -3,14 +3,17 @@
 # In this section we implement the tools necessary for gradient descent, and the gradient descent algorithm
 
 # gradient wrt the parameters of the model
-function gradient_param(m::AbstractModel{S}, val::ValuationPolydisc{S, T},
-    v::ValuationTangent{S, T}) where S where T
+function gradient_param(
+    m::AbstractModel{S},
+    val::ValuationPolydisc{S,T},
+    v::ValuationTangent{S,T}
+) where S where T
     # TODO: this doesn't allow arbitrary shapes for the variable of the model (i.e.
     # this only works if the parameters are the last variables.
     # Do we really need to have something more general?
     new_base = concatenate(val, v.point)
-    new_direction = [val.center ; v.direction]
-    new_v = ValuationTangent(new_base, new_direction, [zeros(T, dim(val)) ; v.magnitude])
+    new_direction = [val.center; v.direction]
+    new_v = ValuationTangent(new_base, new_direction, [zeros(T, dim(val)); v.magnitude])
     grad_indices = (dim(val)+1):(dim(val)+dim(v))
     ## CHANGE ME!
     return partial_gradient(m.fun, new_v, grad_indices)
@@ -22,8 +25,12 @@ function gradient_data(m::Model, data)
 end
 
 # This is the optimiser function used for the "Gradient Descent" OptimSetup structure.
-# The loss is already a closure over the data, so we don't need to pass data separately
-function gradient_descent(loss::Loss, param::ValuationPolydisc{S, T}, degree=1) where S where T
+function gradient_descent(
+    loss::Loss,
+    param::ValuationPolydisc{S,T},
+    state::U,
+    degree::Int
+) where S where T where U
     # Compute the children of the point param
     below_nodes = children(param, degree)
     # Get the corresponding tangent vectors
@@ -31,10 +38,22 @@ function gradient_descent(loss::Loss, param::ValuationPolydisc{S, T}, degree=1) 
     # In gradient descent, we look at the children of the current parameter point and take the child
     # that maximises the norm of the (downwards pointing) gradient
     val, ind = findmax([LinearAlgebra.norm(loss.grad(v)) for v in tangents])
-    return below_nodes[ind]
+    return below_nodes[ind], state
 end
 
 # Helper function to initialise the "Gradient Descent" OptimSetup structure.
-function gradient_descent_init(param::ValuationPolydisc{S, T}, loss::Loss, degree=1) where S where T
-    return OptimSetup(loss, param, (l, p) -> gradient_descent(l, p, degree))
+# Note that here the state does nothing. 
+function gradient_descent_init(
+    param::ValuationPolydisc{S,T},
+    loss::Loss,
+    state::U,
+    degree=1
+) where S where T where U
+    return OptimSetup(
+        loss,
+        param,
+        (l, p, st, ctx) -> gradient_descent(l, p, st, ctx),
+        state,
+        degree
+    )
 end
--- src/optim/greedy_descent.jl
+++ src/optim/greedy_descent.jl
@@ -3,18 +3,44 @@
 # In this section we implement greedy descent algorithms
 
 # The optimiser function for greedy descent
-# The loss is already a closure over the data, so we don't need to pass data or model separately
-function greedy_descent(loss::Loss, param::ValuationPolydisc{S, T}, degree=1) where S where T
-    below_nodes = children(param, degree)
+function greedy_descent(
+    loss::Loss, 
+    param::ValuationPolydisc{S,T},
+    next_branch::Int,
+    settings::Tuple{Bool, Int}
+) where S where T where U
+    (strict, degree) = settings
+    if strict
+        below_nodes = children_along_branch(param, next_branch)
+        next_branch = (next_branch + 1) % prime(p)
+    else
+        below_nodes = children(param, degree)
+    end 
     # In greedy descent, we look at the children of the
     # current parameter point and take the child
     # that minimises the loss
-    return argmin(p -> loss.eval(p), below_nodes)
+    return (argmin(p -> loss.eval(p), below_nodes), next_branch)
 end
 
+# function strict_greedy_descent(loss::Loss, param::ValuationPolydisc{S,T}, degree=1) where S where T
+    
+
+# end
+
 # This function outputs the OptimSetup object for greedy descent
-function greedy_descent_init(param::ValuationPolydisc{S, T}, loss::Loss, degree=1) where S where T
-    return OptimSetup(loss, param, (l, p) -> greedy_descent(l, p, degree))
+function greedy_descent_init(
+    param::ValuationPolydisc{S,T}, 
+    loss::Loss,
+    next_branch::Int,
+    settings::Tuple{Bool, Int}
+) where S where T
+    return OptimSetup(
+        loss, 
+        param, 
+        (l, p, st, ctx) -> greedy_descent(l, p, st, ctx), 
+        next_branch, 
+        settings
+    )
 end
 
 # TODO: Implement the 1D tree search algorithm
