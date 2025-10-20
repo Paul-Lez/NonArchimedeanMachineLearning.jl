8c2ea0168c3f547cc4887418285d181e7039ea22
Paul-Lez <paul.lezeau@gmail.com>
2025-10-20 12:18:32 +0100

    Fix other bug

--- src/optim/greedy_descent.jl
+++ src/optim/greedy_descent.jl
@@ -4,18 +4,19 @@
 
 # The optimiser function for greedy descent
 function greedy_descent(
-    loss::Loss, 
+    loss::Loss,
     param::ValuationPolydisc{S,T},
     next_branch::Int,
-    settings::Tuple{Bool, Int}
+    settings::Tuple{Bool,Int}
 ) where S where T where U
     (strict, degree) = settings
     if strict
         below_nodes = children_along_branch(param, next_branch)
-        next_branch = next_branch == prime(param) ? (next_branch + 1) : 1
+        next_branch = next_branch == dim(param) ? 1 : next_branch + 1
+        # println("$(next_branch)")
     else
         below_nodes = children(param, degree)
-    end 
+    end
     # In greedy descent, we look at the children of the
     # current parameter point and take the child
     # that minimises the loss
@@ -23,22 +24,22 @@ function greedy_descent(
 end
 
 # function strict_greedy_descent(loss::Loss, param::ValuationPolydisc{S,T}, degree=1) where S where T
-    
+
 
 # end
 
 # This function outputs the OptimSetup object for greedy descent
 function greedy_descent_init(
-    param::ValuationPolydisc{S,T}, 
+    param::ValuationPolydisc{S,T},
     loss::Loss,
     next_branch::Int,
-    settings::Tuple{Bool, Int}
+    settings::Tuple{Bool,Int}
 ) where S where T
     return OptimSetup(
-        loss, 
-        param, 
-        (l, p, st, ctx) -> greedy_descent(l, p, st, ctx), 
-        next_branch, 
+        loss,
+        param,
+        (l, p, st, ctx) -> greedy_descent(l, p, st, ctx),
+        next_branch,
         settings
     )
 end
