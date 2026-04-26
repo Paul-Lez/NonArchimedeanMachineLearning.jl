4998b83d8d50578873b8b7c3ad3af1b9e1ffdb08
Paul Lezeau <paul.lezeau@gmail.com>
2026-04-26 22:14:08 +0100

    Fix reconnected test regressions

--- test/geodesic.jl
+++ test/geodesic.jl
@@ -13,8 +13,8 @@ using NonArchimedeanMachineLearning
     # Test that conversion functions are inverses
     test_vals = [0, 1, 2, 5, 10]
     for val in test_vals
-        r = valuation_to_radius(val, p)
-        val_back = radius_to_valuation(r, p)
+        r = NonArchimedeanMachineLearning.valuation_to_radius(val, p)
+        val_back = NonArchimedeanMachineLearning.radius_to_valuation(r, p)
         @test abs(val - val_back) < 1e-10
     end
 
@@ -22,22 +22,22 @@ using NonArchimedeanMachineLearning
     d1 = ValuationPolydisc([K(0)], [5])
     d2 = ValuationPolydisc([K(0)], [2])
 
-    d_0 = geodesic_interpolation(d1, d2, 0.0)
-    r1_actual = valuation_to_radius(5, p)
+    d_0 = NonArchimedeanMachineLearning.geodesic_interpolation(d1, d2, 0.0)
+    r1_actual = NonArchimedeanMachineLearning.valuation_to_radius(5, p)
     @test abs(d_0.radius[1] - r1_actual) < 1e-10
 
-    d_1 = geodesic_interpolation(d1, d2, 1.0)
-    r2_actual = valuation_to_radius(2, p)
+    d_1 = NonArchimedeanMachineLearning.geodesic_interpolation(d1, d2, 1.0)
+    r2_actual = NonArchimedeanMachineLearning.valuation_to_radius(2, p)
     @test abs(d_1.radius[1] - r2_actual) < 1e-10
 
-    d_mid = geodesic_interpolation(d1, d2, 0.5)
+    d_mid = NonArchimedeanMachineLearning.geodesic_interpolation(d1, d2, 0.5)
     r_mid_expected = 0.5 * r1_actual + 0.5 * r2_actual
     @test abs(d_mid.radius[1] - r_mid_expected) < 1e-10
 
     x_values = [0.0, 0.25, 0.5, 0.75, 1.0]
     radii = Float64[]
     for x in x_values
-        d = geodesic_interpolation(d1, d2, x)
+        d = NonArchimedeanMachineLearning.geodesic_interpolation(d1, d2, x)
         push!(radii, d.radius[1])
     end
     for i in 1:(length(radii) - 1)
@@ -47,11 +47,11 @@ using NonArchimedeanMachineLearning
     d1_2d = ValuationPolydisc([K(0), K(1)], [4, 3])
     d2_2d = ValuationPolydisc([K(0), K(1)], [1, 1])
 
-    d_mid_2d = geodesic_interpolation(d1_2d, d2_2d, 0.5)
-    r1_coord1 = valuation_to_radius(4, p)
-    r2_coord1 = valuation_to_radius(1, p)
-    r1_coord2 = valuation_to_radius(3, p)
-    r2_coord2 = valuation_to_radius(1, p)
+    d_mid_2d = NonArchimedeanMachineLearning.geodesic_interpolation(d1_2d, d2_2d, 0.5)
+    r1_coord1 = NonArchimedeanMachineLearning.valuation_to_radius(4, p)
+    r2_coord1 = NonArchimedeanMachineLearning.valuation_to_radius(1, p)
+    r1_coord2 = NonArchimedeanMachineLearning.valuation_to_radius(3, p)
+    r2_coord2 = NonArchimedeanMachineLearning.valuation_to_radius(1, p)
     r_mid_coord1 = 0.5 * r1_coord1 + 0.5 * r2_coord1
     r_mid_coord2 = 0.5 * r1_coord2 + 0.5 * r2_coord2
 
@@ -62,28 +62,28 @@ using NonArchimedeanMachineLearning
     d1_3 = ValuationPolydisc([K3(0)], [4])
     d2_3 = ValuationPolydisc([K3(0)], [1])
 
-    d_mid_3 = geodesic_interpolation(d1_3, d2_3, 0.5)
-    r1_3 = valuation_to_radius(4, 3)
-    r2_3 = valuation_to_radius(1, 3)
+    d_mid_3 = NonArchimedeanMachineLearning.geodesic_interpolation(d1_3, d2_3, 0.5)
+    r1_3 = NonArchimedeanMachineLearning.valuation_to_radius(4, 3)
+    r2_3 = NonArchimedeanMachineLearning.valuation_to_radius(1, 3)
     r_mid_3 = 0.5 * r1_3 + 0.5 * r2_3
     @test abs(d_mid_3.radius[1] - r_mid_3) < 1e-10
 
     d1_center = ValuationPolydisc([K(7)], [5])
     d2_center = ValuationPolydisc([K(7)], [2])
 
-    d_interp = geodesic_interpolation(d1_center, d2_center, 0.3)
+    d_interp = NonArchimedeanMachineLearning.geodesic_interpolation(d1_center, d2_center, 0.3)
     @test NonArchimedeanMachineLearning.valuation(d_interp.center[1] - d1_center.center[1]) > 15
 
     d_small = ValuationPolydisc([K(0)], [5])
     d_not_containing = ValuationPolydisc([K(0)], [6])
-    @test_throws Exception geodesic_interpolation(d_small, d_not_containing, 0.5)
+    @test_throws Exception NonArchimedeanMachineLearning.geodesic_interpolation(d_small, d_not_containing, 0.5)
 
     d_center1 = ValuationPolydisc([K(0)], [5])
     d_center2 = ValuationPolydisc([K(1)], [1])
-    @test_throws Exception geodesic_interpolation(d_center1, d_center2, 0.5)
+    @test_throws Exception NonArchimedeanMachineLearning.geodesic_interpolation(d_center1, d_center2, 0.5)
 
     d1_bounds = ValuationPolydisc([K(0)], [5])
     d2_bounds = ValuationPolydisc([K(0)], [2])
-    @test_throws Exception geodesic_interpolation(d1_bounds, d2_bounds, -0.1)
-    @test_throws Exception geodesic_interpolation(d1_bounds, d2_bounds, 1.1)
+    @test_throws Exception NonArchimedeanMachineLearning.geodesic_interpolation(d1_bounds, d2_bounds, -0.1)
+    @test_throws Exception NonArchimedeanMachineLearning.geodesic_interpolation(d1_bounds, d2_bounds, 1.1)
 end
